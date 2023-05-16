16c8ff475101a23b89f2456f0e2e764f4d506bce
Bugoy <47383234+svperbugoy@users.noreply.github.com>
2023-05-16 20:34:37 +0400

    Update serverIP.sh

--- serverIP.sh
+++ serverIP.sh
@@ -1,7 +1,7 @@
 ########################################################################################
 # Display SERVER IP
 echo "" >> /etc/issue
-echo -e "eth0: \4{enp0s3}" >> /etc/issue
+echo eth0: "\4{enp0s3}" >> /etc/issue
 echo "IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')" >> /etc/rc.local
 echo ""eth0 IP: $IP" > /etc/issue" >> /etc/rc.local
 echo "IP Displayed"
