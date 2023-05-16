b94fa225ae53aa07b30462777c7e7ea80a176bdc
Bugoy <47383234+svperbugoy@users.noreply.github.com>
2023-05-16 14:37:21 +0400

    Update buster.sh

--- buster.sh
+++ buster.sh
@@ -82,10 +82,10 @@ echo "Docker Installed"
 ########################################################################################
 # Install PORTAINER (9000)
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 -p 9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
+docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
 echo "Portainer Installed"
 ########################################################################################
 
-
+Sudo reboot
 
 
