2bfd3ebaffdd3421b73cd8de6d7fe190f0ea785a
Bugoy <47383234+svperbugoy@users.noreply.github.com>
2023-05-16 20:27:25 +0400

    Create serverIP.sh

---
+++ serverIP.sh
@@ -0,0 +1,10 @@
+########################################################################################
+# Display SERVER IP
+echo "" >> /etc/issue
+echo "eth0: \4{enp0s3}" >> /etc/issue
+echo "IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')" >> /etc/rc.local
+echo ""eth0 IP: $IP" > /etc/issue" >> /etc/rc.local
+echo "IP Displayed"
+########################################################################################
+
+sudo reboot
