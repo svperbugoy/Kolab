9815bc2c837732e9134825d400974d6f4f5ea5ef
Bugoy <47383234+svperbugoy@users.noreply.github.com>
2023-05-16 20:21:20 +0400

    Update buster.sh

--- buster.sh
+++ buster.sh
@@ -90,9 +90,14 @@ echo "Portainer Installed"
 # Display SERVER IP
 echo "" >> /etc/issue
 echo "eth0: \4{enp0s3}" >> /etc/issue
+echo "IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')" >> /etc/rc.local
+echo ""eth0 IP: $IP" > /etc/issue" >> /etc/rc.local
 echo "IP Displayed"
 ########################################################################################
 
-reboot
+
+
+
+sudo reboot
 
 
